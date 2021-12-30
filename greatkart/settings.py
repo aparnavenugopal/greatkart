0f465bb27be7a604b7cd36d449fe62848dfc5577
Aparna <aparnavenugopal1996@gmail.com>
2021-12-30 13:56:03 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -134,3 +136,16 @@ STATICFILES_DIRS=[
 #media files configuration
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR /'media'
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+
+}
+
+#smtp configuration
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='aparnavenugopal1996@gmail.com'
+EMAIL_HOST_PASSWORD='Aparna430#'
+EMAIL_USE_TLS=True
